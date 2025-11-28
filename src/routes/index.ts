6e58b5ed9db6ef609f30755c7e3ae241af8ac251
crissalves <cristianalves_cg@hotmail.com>
2025-11-28 11:15:19 -0300

    Entidades e testes ok

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import employeeRoutes from "./employee.route"
+import employeeRoutes from "./employee.route";
+import { serviceRoutes } from "./serviceRequest.routes";
 
 const routes = Router();
 
 routes.use("/employee", employeeRoutes);
+routes.use("/services", serviceRoutes);
 
 export default routes;
